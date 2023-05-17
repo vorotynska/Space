dccae73d49926ccb46f1b89f202928e72a0866a2
Irina Carpincic <iryna.vorotynska@gmail.com>
2023-05-17 23:52:42 +0200

    commit

--- tabs.js
+++ tabs.js
@@ -16,24 +16,21 @@ function changeTabFocus(e) {
 
     if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
         tabs[tabFocus].setAttribute("tabindex", -1);
-    }
 
-    if (e.keyCode === keydownRight) {
-        tabFocus++;
-        if (tabFocus >= tabs.length) {
-            tabFocus = 0;
+        if (e.keyCode === keydownRight) {
+            tabFocus++;
+            if (tabFocus >= tabs.length) {
+                tabFocus = 0;
+            }
+        } else if (e.keyCode === keydownLeft) {
+            tabFocus--;
+            if (tabFocus < 0) {
+                tabFocus = tabs.length - 1;
+            }
         }
+        tabs[tabFocus].setAttribute("tabindex", 0);
+        tabs[tabFocus].focus();
     }
-
-    if (e.keyCode === keydownLeft) {
-        tabFocus--;
-        if (tabFocus < 0) {
-            tabFocus = tabs.length - 1;
-        }
-    }
-
-    tabs[tabFocus].setAttribute("tabindex", 0);
-    tabs[tabFocus].focus();
 }
 
 
@@ -51,18 +48,19 @@ function changeTabPanel(e) {
 
     targetTab.setAttribute("aria-selected", true);
 
-    mainContainer
-        .querySelectorAll('[role="tabpanel"]')
-        .forEach((panel) => panel.setAttribute("hidden", true));
+    hideContent(mainContainer, '[role="tabpanel"]');
+    showContent(mainContainer, [`#${targetPanel}`]);
 
-    mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
+    hideContent(mainContainer, 'picture');
+    showContent(mainContainer, [`#${targetImage}`]);
+}
 
-    mainContainer
-        .querySelectorAll('picture')
-        .forEach((picture) => picture.setAttribute("hidden", true));
+function hideContent(parent, content) {
+    parent
+        .querySelectorAll(content)
+        .forEach((item) => item.setAttribute("hidden", true));
+}
 
-    mainContainer.querySelector([`#${targetImage}`]).removeAttribute('hidden');
-
-
-    // console.log(mainContainer);
+function showContent(parent, content) {
+    parent.querySelector(content).removeAttribute('hidden');
 }
