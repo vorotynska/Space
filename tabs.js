a8bc7472da8edc607c7a95420d6a8d3b60997833
Irina Carpincic <iryna.vorotynska@gmail.com>
2023-05-17 20:16:39 +0200

    commit

--- tabs.js
+++ tabs.js
@@ -1,9 +1,12 @@
 const tabList = document.querySelector('[role="tablist"]');
 const tabs = tabList.querySelectorAll('[role="tab"]');
 
-
-
 tabList.addEventListener('keydown', changeTabFocus);
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', changeTabPanel);
+});
+
 
 let tabFocus = 0;
 
@@ -31,4 +34,35 @@ function changeTabFocus(e) {
 
     tabs[tabFocus].setAttribute("tabindex", 0);
     tabs[tabFocus].focus();
+}
+
+
+function changeTabPanel(e) {
+    const targetTab = e.target;
+    const targetPanel = targetTab.getAttribute("aria-controls");
+    const targetImage = targetTab.getAttribute("data-image");
+
+    const tabContainer = targetTab.parentNode;
+    const mainContainer = tabContainer.parentNode;
+
+    tabContainer
+        .querySelector('[aria-selected="true"]')
+        .setAttribute("aria-selected", false);
+
+    targetTab.setAttribute("aria-selected", true);
+
+    mainContainer
+        .querySelectorAll('[role="tabpanel"]')
+        .forEach((panel) => panel.setAttribute("hidden", true));
+
+    mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
+
+    mainContainer
+        .querySelectorAll('picture')
+        .forEach((picture) => picture.setAttribute("hidden", true));
+
+    mainContainer.querySelector([`#${targetImage}`]).removeAttribute('hidden');
+
+
+    // console.log(mainContainer);
 }
